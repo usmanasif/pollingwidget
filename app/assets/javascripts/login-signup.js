9376583ebf22de571ab0f1f001f84929355ae4b0
Im Genius <im.genius.man@gmail.com>
2017-09-12 13:09:22 +0500

    preview question setup * form preview plugin added * image preview js code added * all fields preview with image working

--- app/assets/javascripts/login-signup.js
+++ app/assets/javascripts/login-signup.js
@@ -1,25 +1,25 @@
-$(document).on('click', '.signup', function(e) {
-  $('#login').addClass('hide');
-  $('#register').removeClass('hide');
-});
-$(document).on('click', '.login', function(e) {
-  $('#login').removeClass('hide');
-  $('#register').addClass('hide');
-});
 $(document).on("turbolinks:load",function() {
   return setTimeout((function() {
     return $('.alert').slideUp();
   }), 3000);
 });
-$(document).on('click', '#multiple-choice', function() {
-  $("#multiple").show();
-  $("#truefalse").hide();
-});
-$(document).on('click', '#true-false', function() {
-  $("#truefalse").show();
-  $("#multiple").hide();
-});
-$(document).on('ready', function () {
+$(document).on("turbolinks:load",function() {
+  $(document).on('click', '.signup', function(e) {
+    $('#login').addClass('hide');
+    $('#register').removeClass('hide');
+  });
+  $(document).on('click', '.login', function(e) {
+    $('#login').removeClass('hide');
+    $('#register').addClass('hide');
+  });
+  $(document).on('click', '#multiple-choice', function() {
+    $(".multiple").show();
+    $(".truefalse").hide();
+  });
+  $(document).on('click', '#true-false', function() {
+    $(".truefalse").show();
+    $(".multiple").hide();
+  });
   $(".question-fields :input").attr("disabled", true);
   $( ".question-select" ).attr("disabled", true);
 
@@ -29,4 +29,23 @@ $(document).on('ready', function () {
   $( ".question-select" ).change(function() {
     $(".question-fields :input").attr("disabled", false);
   });
+      $('.preview-block').preview({
+    form: '#form'
 });
+});
+function readURL(input) {
+        if (input.files && input.files[0]) {
+          var reader = new FileReader();
+
+          reader.onload = function (e) {
+            $('#img_prev')
+              .attr('src', e.target.result)
+              .width(130)
+              .height(105);
+          };
+
+          reader.readAsDataURL(input.files[0]);
+        }
+        $('#img_prev').removeClass('hide');
+      }
+
